3cb9099fd0f89044441ed455a68a00547bde339e
Scott Colby <scolby33@gmail.com>
2015-10-29 20:45:27 -0700

    Use test values for instantiation of PushoverNotifierBackend in tests

--- tests/NotifierBackend/test_PushoverNotifierBackend.py
+++ tests/NotifierBackend/test_PushoverNotifierBackend.py
@@ -2,8 +2,12 @@ import pytest
 
 from event_notifier.NotifierBackend import ANotifierBackend, PushoverNotifierBackend
 
+TEST_TOKEN = 'a'
+TEST_USER = 'b'
+TEST_DEVICE = 'c'
+
 def test_PushoverNotifierBackend_exists():
-    assert isinstance(PushoverNotifierBackend(None, None), PushoverNotifierBackend)
+    assert isinstance(PushoverNotifierBackend(TEST_TOKEN, TEST_USER), PushoverNotifierBackend)
     
 def test_PushoverNotifierBackend_is_an_ANotifierBackend():
-    assert isinstance(PushoverNotifierBackend(None, None), ANotifierBackend)
+    assert isinstance(PushoverNotifierBackend(TEST_TOKEN, TEST_USER), ANotifierBackend)
