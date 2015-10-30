5f18fa409f6840991d4c783a056d2499ed26d274
Scott Colby <scolby33@gmail.com>
2015-10-29 18:41:51 -0700

    Add __all__ variable to __init__.py

--- src/event_notifier/__init__.py
+++ src/event_notifier/__init__.py
@@ -5,3 +5,11 @@ from . import Notification
 
 from .StorageBackend import AStorageBackend
 from .NotifierBackend import ANotifierBackend, PushoverNotifierBackend
+
+__all__ = [
+    'Event',
+    'Notification',
+    'AStorageBackend',
+    'ANotifierBackend',
+    'PushoverNotifierBackend'
+]
