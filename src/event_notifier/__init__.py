557d7d3bc57096e246069ac9d57dfc0c8fe3ccf6
Scott Colby <scolby33@gmail.com>
2015-10-29 18:42:01 -0700

    Add metadata to __init__.py

--- src/event_notifier/__init__.py
+++ src/event_notifier/__init__.py
@@ -6,6 +6,20 @@ from . import Notification
 from .StorageBackend import AStorageBackend
 from .NotifierBackend import ANotifierBackend, PushoverNotifierBackend
 
+
+__version__ = '0.0.1'
+
+__title__ = 'event_notifier'
+__description__ = 'A Python3 package for unified processing and dispatching of various notifications.'
+__url__ = 'https://github.com/scolby33/event_notifier'
+
+__author__ = 'Scott Colby'
+__email__ = ''
+
+__license__ = 'Other/Proprietary License'
+__copyright__ = 'Copyright (c) 2015 Scott Colby'
+
+
 __all__ = [
     'Event',
     'Notification',
