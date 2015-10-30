49c7b1768c1155814ec710eebdf35a8f49183d56
Scott Colby <scolby33@gmail.com>
2015-10-29 20:09:40 -0700

    Add required libraries to setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
     'Topic :: Communications'
 ]
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = ['attrs', 'requests']
 EXTRAS_REQUIRE = {}
 
 #################################################################
