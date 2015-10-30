6672ac958707173f28d9b101575a43ce65cde8d5
Scott Colby <scolby33@gmail.com>
2015-10-29 20:10:35 -0700

    Add first test -- for ANotifierBackend

---
+++ tests/NotifierBackend/test_ANotifierBackend.py
@@ -0,0 +1,7 @@
+import pytest
+
+from event_notifier.NotifierBackend import ANotifierBackend
+
+def test_ANotifierBackend_cannot_be_instantiated():
+    with pytest.raises(TypeError):
+        ANotifierBackend()
