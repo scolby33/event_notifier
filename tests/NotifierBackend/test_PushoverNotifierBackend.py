a3b90cbb1a4b568fecd0317a132b650373167883
Scott Colby <scolby33@gmail.com>
2015-10-29 20:45:48 -0700

    Test PushoverNotifierBackend's instantiation

--- tests/NotifierBackend/test_PushoverNotifierBackend.py
+++ tests/NotifierBackend/test_PushoverNotifierBackend.py
@@ -11,3 +11,9 @@ def test_PushoverNotifierBackend_exists():
     
 def test_PushoverNotifierBackend_is_an_ANotifierBackend():
     assert isinstance(PushoverNotifierBackend(TEST_TOKEN, TEST_USER), ANotifierBackend)
+
+def test_PushoverNotifierBackend_accepts_init_arguments():
+    testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
+    assert testPushoverNotifierBackend.token == TEST_TOKEN
+    assert testPushoverNotifierBackend.user == TEST_USER
+    assert testPushoverNotifierBackend.device == TEST_DEVICE
