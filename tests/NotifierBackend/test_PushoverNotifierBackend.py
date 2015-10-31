643dba5b9a865ee502e978abaef350ebeeee0d8c
Scott Colby <scolby33@gmail.com>
2015-10-31 03:02:57 -0700

    Add some tests for dispatch_message in the PushoverNotifierBackend
    Next step—bad Notifications

--- tests/NotifierBackend/test_PushoverNotifierBackend.py
+++ tests/NotifierBackend/test_PushoverNotifierBackend.py
@@ -1,6 +1,9 @@
 import pytest
+import responses
 
 from event_notifier.NotifierBackend import ANotifierBackend, PushoverNotifierBackend
+from event_notifier.Notification import Notification
+from MockPushoverServer import MockPushoverServer
 from tests.constants import *
 
 def test_PushoverNotifierBackend_exists():
@@ -14,3 +17,23 @@ def test_PushoverNotifierBackend_accepts_init_arguments():
     assert testPushoverNotifierBackend.token == TEST_TOKEN
     assert testPushoverNotifierBackend.user == TEST_USER
     assert testPushoverNotifierBackend.device == TEST_DEVICE
+
+class StartMockPushoverServer(MockPushoverServer):
+    @responses.activate
+    def test_PushoverNotifierBackend_sends_valid_messages():
+        testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
+        testSimpleNotification = Notification(
+            subject = TEST_SUBJECT,
+            message = TEST_MESSAGE
+        )
+        testComplexNotification = Notification(
+            subject = TEST_SUBJECT,
+            message = TEST_MESSAGE,
+            url = TEST_URL,
+            url_title = TEST_URL_TITLE,
+            priority = TEST_PRIORITY,
+            timestamp = TEST_TIMESTAMP,
+            sound = TEST_SOUND
+        )
+        testPushoverNotifierBackend.dispatch_notification(testSimpleNotification)
+        testPushoverNotifierBackend.dispatch_notification(testComplexNotification)
