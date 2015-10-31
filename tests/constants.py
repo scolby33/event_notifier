d7d89fa3e742c95fda7119672c5f2937a37825ae
Scott Colby <scolby33@gmail.com>
2015-10-31 01:48:59 -0700

    Add test constants for unification of constant values used in testing

---
+++ tests/constants.py
@@ -0,0 +1,26 @@
+import datetime
+
+TEST_TOKEN = 'axK4aK9HL9XL7TNNRTtK69ukNkdLhV'
+TEST_USER = 'uHAP7bA4h9CrmyennixemR0L3nqYgM'
+TEST_DEVICE = 'my-phone'
+
+TEST_SUBJECT = 'Eager Elder'
+TEST_MESSAGE = 'The alternate wind sustains the lay history.'
+TEST_URL = 'http://example.com/ValidFishing'
+TEST_URL_TITLE = 'Valid Fishing'
+TEST_PRIORITY = 1
+TEST_TIMESTAMP = datetime.datetime(2015, 1, 1, 12, 31, 4, 14)
+TEST_SOUND = 'towel'
+
+__all__ = [
+   'TEST_TOKEN',
+   'TEST_USER',
+   'TEST_DEVICE',
+   'TEST_SUBJECT',
+   'TEST_MESSAGE',
+   'TEST_URL',
+   'TEST_URL_TITLE',
+   'TEST_PRIORITY',
+   'TEST_TIMESTAMP',
+   'TEST_SOUND' 
+]
