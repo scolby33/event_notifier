795a8be605c9e10c51c91827643460fe5acce40d
Scott Colby <scolby33@gmail.com>
2015-11-01 03:10:14 -0800

    Add new fixtures to test.fixtures.__init__

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,5 +1,10 @@
-from .mock_pushover_server import mock_pushover_server
+from .mock_pushover_server import mock_pushover_server, mock_bad_pushover_server
+from .mock_notification import mock_simple_notification, mock_complex_notification, mock_bad_notification
 
 __all__ = [
-    'mock_pushover_server'
+    'mock_pushover_server',
+    'mock_bad_pushover_server',
+    'mock_simple_notification',
+    'mock_complex_notification',
+    'mock_bad_notification',
 ]
