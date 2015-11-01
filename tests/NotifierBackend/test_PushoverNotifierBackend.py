77daa1c44c02496116ccad2b50426bfe84b65088
Scott Colby <scolby33@gmail.com>
2015-11-01 03:11:40 -0800

    Switch to using fixtures and add tests for bad requests and bad servers

--- tests/NotifierBackend/test_PushoverNotifierBackend.py
+++ tests/NotifierBackend/test_PushoverNotifierBackend.py
@@ -1,9 +1,11 @@
 import pytest
 import responses
 
+from event_notifier.exceptions import EventNotifierNotificationDispatchException
 from event_notifier.NotifierBackend import ANotifierBackend, PushoverNotifierBackend
 from event_notifier.Notification import Notification
-from tests.fixtures import mock_pushover_server
+
+from tests.fixtures import mock_pushover_server, mock_bad_pushover_server, mock_simple_notification, mock_complex_notification, mock_bad_notification
 from tests.constants import *
 
 def test_PushoverNotifierBackend_exists():
@@ -19,24 +21,29 @@ def test_PushoverNotifierBackend_accepts_init_arguments():
     assert testPushoverNotifierBackend.device == TEST_DEVICE
 
 @responses.activate
-def test_PushoverNotifierBackend_sends_valid_simple_notification(mock_pushover_server):
+def test_PushoverNotifierBackend_sends_valid_simple_notification(mock_pushover_server, mock_simple_notification):
     testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
-    testSimpleNotification = Notification(
-        subject = TEST_SUBJECT,
-        message = TEST_MESSAGE
-    )        
-    assert testPushoverNotifierBackend.dispatch_notification(testSimpleNotification)
-    
+    assert testPushoverNotifierBackend.dispatch_notification(mock_simple_notification)
+
 @responses.activate
-def test_PushoverNotifierBackend_sends_valid_complex_notification(mock_pushover_server):
+def test_PushoverNotifierBackend_sends_valid_complex_notification(mock_pushover_server, mock_complex_notification):
     testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
-    testComplexNotification = Notification(
-        subject = TEST_SUBJECT,
-        message = TEST_MESSAGE,
-        url = TEST_URL,
-        url_title = TEST_URL_TITLE,
-        priority = TEST_PRIORITY,
-        timestamp = TEST_TIMESTAMP,
-        sound = TEST_SOUND
-    )
-    assert testPushoverNotifierBackend.dispatch_notification(testComplexNotification)
+    assert testPushoverNotifierBackend.dispatch_notification(mock_complex_notification)
+
+@responses.activate
+def test_PushoverNotifierBackend_raises_exception_when_a_bad_notification_is_provided(mock_pushover_server, mock_bad_notification):
+    testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
+    with pytest.raises(AttributeError):
+        testPushoverNotifierBackend.dispatch_notification(mock_bad_notification)
+
+def test_PushoverNotifierBackend_raises_exception_when_server_rejects_notification(mock_pushover_server, mock_simple_notification):
+    testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN + 'bad', TEST_USER, TEST_DEVICE)
+    with pytest.raises(EventNotifierNotificationDispatchException):
+        testPushoverNotifierBackend.dispatch_notification(mock_simple_notification)
+
+@responses.activate
+def test_PushoverNotifierBackend_raises_exception_when_server_down(mock_bad_pushover_server, mock_simple_notification):
+    testPushoverNotifierBackend = PushoverNotifierBackend(TEST_TOKEN, TEST_USER, TEST_DEVICE)
+    with pytest.raises(EventNotifierNotificationDispatchException):
+        testPushoverNotifierBackend.dispatch_notification(mock_simple_notification)
+    assert False
