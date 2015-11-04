37f966c7f09204162c0a33ee66b7b4c422f394af
Scott Colby <scolby33@gmail.com>
2015-11-03 17:25:10 -0800

    AStorageBackend implementation

---
+++ src/event_notifier/StorageBackend.py
@@ -0,0 +1,41 @@
+import abc
+import os
+
+import sqlalchemy.ext.declarative
+
+class AStorageBackend(object,  metaclass=abc.ABCMeta):
+    """Abstract class for storage backends
+    
+    :param storage_location: the location where the database or other storage medium is stored
+    """
+    @abc.abstractmethod
+    def __init__(self, storage_location): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def register_notifier_backend_parameters(self, parameters): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def add_event(self, event): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def remove_event(self, event_id): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def _list_events(self): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def add_notification(self, notification): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def remove_notification(self, notification_id): # pragma: no cover
+        raise NotImplementedError
+        
+    @abc.abstractmethod
+    def _list_notifications(self): # pragma: no cover
+        raise NotImplementedError
