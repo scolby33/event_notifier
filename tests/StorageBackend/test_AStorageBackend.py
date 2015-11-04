46d629f0ad5a627e13432ef69f41889c03d3382a
Scott Colby <scolby33@gmail.com>
2015-11-03 17:18:37 -0800

    Tests for AStorageBackend class

---
+++ tests/StorageBackend/test_AStorageBackend.py
@@ -0,0 +1,7 @@
+import pytest
+
+from event_notifier.StorageBackend import AStorageBackend
+
+def test_AStorageBackend_cannot_be_instantiated():
+    with pytest.raises(TypeError):
+        AStorageBackend()
